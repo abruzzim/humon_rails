152eeb8a2771be0dbf37edc0be796f4ebe844763
abruzzim <abruzzim@framework-solutions.com>
2015-02-08 21:36:05 -0500

    Add Route API Version Scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
-Rails.application.routes.draw do
+Humon::Application.routes.draw do
+	scope module: :api, defaults: { format: 'json' } do
+		namespace :v1 do
+			# Add resources here
+		end
+	end
 end
