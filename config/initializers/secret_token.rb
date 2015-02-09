7ff0ac2ecf9f8a0d1bab4c630ef73028d71bdc70
abruzzim <abruzzim@framework-solutions.com>
2015-02-09 00:09:34 -0500

    Add Secret Token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,13 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+Humon::Application.config.secret_key_base = '1351374f3afbc94143574e45d8baf138d0ac77193f5d537fa08d0f2d587f986e215d3a545a66ed9ecd4b8db7e662a40dacfb2ad7260be032d09acde0bafe8b62'
